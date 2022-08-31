582fda2ae59e1d5ce7eb57e385f1f51459870654
Büşra Yaşar <busrayasar1996@gmail.com>
2022-08-31 06:16:43 +0300

    itunes search app

---
+++ ItunesSearchApp/app/src/main/java/com/busrayasar/itunessearchapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.busrayasar.itunessearchapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.busrayasar.itunessearchapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
